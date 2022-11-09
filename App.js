f550ed4c8416f0e8ffa377eddca8a9d07777c042
Ania <ann.leniart@gmail.com>
2022-11-09 23:14:19 +0100

    API_Key

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const openWeatherKey = 'xxx'
+let url = 'https://api.openweathermap.org/data/2.5/weather?lat=54.44&lon=18.57&appid=openWeatherKey'
+
+
 export default function App() {
   return (
     <View style={styles.container}>
