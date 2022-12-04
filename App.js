f3fddeb4d577404859ba4944f5bd2edef5fbfd74
Ania <ann.leniart@gmail.com>
2022-12-04 21:05:16 +0100

    Background added

--- App.js
+++ App.js
@@ -1,24 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+//import { StyleSheet, Text, View } from 'react-native';
 
-const openWeatherKey = 'xxx'
-let url = 'https://api.openweathermap.org/data/2.5/weather?lat=54.44&lon=18.57&appid=openWeatherKey'
-
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hehe</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  ImageBackground,
+  TextInput,
+  ActivityIndicator,
+  View,
+} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  image: {
+    flex: 1,
+    flexDirection: 'column',
   },
 });
+
+const App = () => {
+  return (
+    <View style={styles.root}>
+      <ImageBackground
+      source={require('./assets/pixel_weather.png')}
+      resizeMode="cover"
+      style={styles.image}>
+
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default App;
